db66a21734fc3c4516acaad7411fbdfb669f9ef5
Дмитрий Моисеенко <lovecloudez@gmail.com>
2025-10-09 17:57:18 +0300

    format date util

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -3,3 +3,9 @@ export const formatDate = (dateString: string): string => {
     const months = ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'];
     return `${day} ${months[parseInt(month) - 1]}`;
 };
+
+export const getDayOfWeek = (dateString: string): string => {
+    const date = new Date(dateString.split('.').reverse().join('-'));
+    const days = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
+    return days[date.getDay()];
+};
